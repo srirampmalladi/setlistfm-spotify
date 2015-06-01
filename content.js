f79d4ecf6ee3dfab51699b79276017aa475d43a4
Sriram Malladi <srirampmalladi@gmail.com>
2015-06-01 17:42:00 -0400

    get track uris properly

--- content.js
+++ content.js
@@ -1,55 +1,53 @@
-function load() {
-  var artistLabel = $('span.label').filter(function() {
-      return $(this).text() === 'Artist';
+(function() {
+  var artistLabel = $('span.label').filter(function () {
+    return $(this).text() === 'Artist';
   });
-  var artist = $(artistLabel.siblings()[0]).find('span').text()
-  console.log('artist', artist);
-  var total = 0
-  var num = 0
-  var dict = {}
+  var artistName = $(artistLabel.siblings()[0]).find('span').text()
+  console.log('artist', artistName);
 
-  function finish() {
-      if(total === num) {
-          console.log(dict)    
-      } else {
-          console.log('not done')
-      }
-      
-  }
+  // Loads uris for given list of track names and calls the callback with an array
+  function loadTrackUris(trackNames, callback) {
+    console.log('trackNames', trackNames);
+    var total = trackNames.length;
+    var trackUris = new Array(trackNames.length);
+    var completed = 0;
 
-  function find(seq, artist, title) {
-      console.log(seq + artist + title);
-      $.ajax("http://ws.spotify.com/search/1/track.json", {
-          data: {
-              'q': title + ' AND ' + artist
-          },
-          success: function(data) {
-              for(var i=0;i<data.tracks.length;i++) {
-                  track = data.tracks[i]
-                  if (track.name === title && track.artists[0].name === artist ) {
-                      num++
-                      dict[seq] = track.href
-                      break;
-                 }
-              }
-              finish();
-          }
+    function doSearch(index, trackName) {
+      var uri = "http://api.spotify.com/v1/search";
+      $.ajax(uri, {
+        data: {
+          'q': 'artist:"' + artistName + '"+' + 'track:"' + trackName + '"',
+          'type': 'track'
+        },
+        success: function (data) {
+          console.log(data);
+          var items = data.tracks.items;
+          trackUris[index] = items.length > 0 ? items[0].uri : "";
+        },
+        error: console.log,
+        complete: function() { complete(index); }
       });
+    }
+
+    function complete(index) {
+      if (trackUris[index] === undefined) {
+        trackUris[index] = "";
+      }
+      completed++;
+      console.log('Finished ' + completed + '/' + total);
+      if (total === completed) {
+        callback(trackUris);
+      }
+    }
+
+    trackNames.each(doSearch);
   }
 
-  var i = 0;
-  var titles = []
+  // Makes a playlist from list of uris
+  function makePlaylist(trackUris) {
+    console.log('trackUris', trackUris);
+  }
 
-  total = $('.songLabel').length
-  $('.songLabel').each(function(index, element) {
-      find(index, artist, $(element).text());
-  });
-}
-console.log('loaded file');
-load();
-
-// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-//  console.log('loaded message', message, sender, sendResponse);
-//  load();
-//});
-
+  var trackNames = $('.songLabel').map(function(i, el) { return $(el).text(); });
+  loadTrackUris(trackNames, makePlaylist);
+})();
