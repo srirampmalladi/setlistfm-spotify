e9e7828bd1abc9e742dec348fb825929b358fb5e
Sriram Malladi <srirampmalladi@gmail.com>
2015-06-02 17:18:50 -0400

    better playlist name

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,23 @@
 (function() {
-  var artistLabel = $('span.label').filter(function () {
-    return $(this).text() === 'Artist';
-  });
-  var artistName = $(artistLabel.siblings()[0]).find('span').text();
+  function getValue(labelName) {
+    var label = $('span.label').filter(function () {
+      return $(this).text() === labelName;
+    });
+    return $(label.siblings()[0]).find('span').text();
+  }
+
+  var artistName = getValue('Artist');
+  var venueName = getValue('Venue');
   var trackNames = $('.songLabel').map(function(i, el) { return $(el).text(); });
+
+  var month = $('.dateBlock .m').text();
+  var day = $('.dateBlock .d').text();
+  var year = $('.dateBlock .y').text();
+  var dateStr = month + " " + day + ", " + year;
+  var date = new Date(month + " " + day + ", " + year);
+  var formattedDate = date.toISOString().split('T')[0];
+
+  var playlistName = artistName + " at " + venueName + ", " + formattedDate;
 
   var trackUris = null;
   var acessToken = null;
@@ -19,7 +33,7 @@
   function authCallback(token) {
     acessToken = token;
     if (trackUris != null) {
-      Globals.createPlaylist(acessToken, trackUris, artistName + ' concert');
+      Globals.createPlaylist(acessToken, trackUris, playlistName);
     }
   }
 
